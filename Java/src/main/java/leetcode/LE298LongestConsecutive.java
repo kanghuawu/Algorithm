77f17cc7cdf3aceeee350cc0bd014424d29217f0
kanghuawu <kang-hua.wu@sjsu.edu>
2017-09-01 00:33:47 -0700

    add LEE298 and LEE351

---
+++ Java/src/main/java/leetcode/LE298LongestConsecutive.java
@@ -0,0 +1,44 @@
+package leetcode;
+import utility.TreeNode;
+
+public class LE298LongestConsecutive {
+    // MY
+    private int maxLen;
+    public int longestConsecutive(TreeNode root) {
+        maxLen = 0;
+        if (root == null)
+            return maxLen;
+        longestConsecutiveHelper(root, root.val, 1);
+        return maxLen;
+
+    }
+    private void longestConsecutiveHelper(TreeNode root, int expect, int curLen) {
+        if (root == null) return;
+        if (root.val == expect) {
+            if (curLen > maxLen)
+                maxLen = curLen;
+            longestConsecutiveHelper(root.left, root.val + 1, curLen + 1);
+            longestConsecutiveHelper(root.right, root.val + 1, curLen + 1);
+        } else {
+            longestConsecutiveHelper(root.left, root.val + 1, 2);
+            longestConsecutiveHelper(root.right, root.val + 1, 2);
+        }
+    }
+
+    // DRY
+//    private int max = 0;
+//    public int longestConsecutive(TreeNode root) {
+//        if(root == null) return 0;
+//        helper(root, 0, root.val);
+//        return max;
+//    }
+//
+//    public void helper(TreeNode root, int cur, int target){
+//        if(root == null) return;
+//        if(root.val == target) cur++;
+//        else cur = 1;
+//        max = Math.max(cur, max);
+//        helper(root.left, cur, root.val + 1);
+//        helper(root.right, cur, root.val + 1);
+//    }
+}
