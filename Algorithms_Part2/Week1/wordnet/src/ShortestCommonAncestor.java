b0175f3d0634df67d7371d212a68c58a52741df0
kanghuawu <kang-hua.wu@sjsu.edu>
2017-09-06 18:46:18 -0700

    wordnest init

---
+++ Algorithms_Part2/Week1/wordnet/src/ShortestCommonAncestor.java
@@ -0,0 +1,30 @@
+public class ShortestCommonAncestor {
+
+   // constructor takes a rooted DAG as argument
+   public ShortestCommonAncestor(Digraph G) {
+
+   }
+
+   // length of shortest ancestral path between v and w
+   public int length(int v, int w) {
+
+   }
+
+   // a shortest common ancestor of vertices v and w
+   public int ancestor(int v, int w) {
+
+   }
+
+   // length of shortest ancestral path of vertex subsets A and B
+   public int length(Iterable<Integer> subsetA, Iterable<Integer> subsetB) {
+
+   }
+
+   // a shortest common ancestor of vertex subsets A and B
+   public int ancestor(Iterable<Integer> subsetA, Iterable<Integer> subsetB) {
+      
+   }
+
+   // do unit testing of this class
+   public static void main(String[] args)
+}
---
+++ Algorithms_Part2/Week1/wordnet/src/WordNet.java
@@ -0,0 +1,33 @@
+public class WordNet {
+
+   // constructor takes the name of the two input files
+   public WordNet(String synsets, String hypernyms) {
+
+   }
+
+   // all WordNet nouns
+   public Iterable<String> nouns() {
+
+   }
+
+   // is the word a WordNet noun?
+   public boolean isNoun(String word) {
+
+   }
+
+   // a synset (second field of synsets.txt) that is a shortest common ancestor
+   // of noun1 and noun2 (defined below)
+   public String sca(String noun1, String noun2) {
+
+   }
+
+   // distance between noun1 and noun2 (defined below)
+   public int distance(String noun1, String noun2) {
+
+   }
+
+   // do unit testing of this class
+   public static void main(String[] args) {
+      
+   }
+}
