99ef80e311027586ae1b8d9902eb8ea2fb89bdd9
kanghuawu <kang-hua.wu@sjsu.edu>
2017-10-30 16:55:14 -0700

    reorg python

--- python/LE002addTwoNumbers.py
+++ python/LE002addTwoNumbers.py
@@ -1,8 +1,4 @@
-# Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
+from utility.ListNode import ListNode
 
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
@@ -16,17 +12,17 @@ class Solution(object):
         inc = False
         newval = 0
         while l1 != None or l2 != None or inc :
-        	newval = 1 if inc else 0
-        	if l1 != None:
-        		newval += l1.val
-        		l1 = l1.next
-        	if l2 != None:
-        		newval += l2.val
-        		l2 = l2.next
-        	inc = True if newval >= 10 else False
-        	node.next = ListNode(newval % 10)
-        	# print str(node.val)
-        	node = node.next
+            newval = 1 if inc else 0
+            if l1 != None:
+                newval += l1.val
+                l1 = l1.next
+            if l2 != None:
+                newval += l2.val
+                l2 = l2.next
+            inc = True if newval >= 10 else False
+            node.next = ListNode(newval % 10)
+            # print(str(node.val))
+            node = node.next
         return dummy.next
 
 l1 = ListNode(1)
@@ -44,10 +40,8 @@ l11.next = l12
 l12.next = l13
 l13.next = l14
 
-def printList(node):
-	while node != None:
-		print str(node.val),
-		node = node.next
 
-test = Solution()
-printList(test.addTwoNumbers(l1, l11))
+if __name__ == '__main__':
+    test = Solution()
+    l = test.addTwoNumbers(l1, l11)
+    l.print()
