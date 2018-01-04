c96c7e6055ae8da55fb537e4b4937d1c6a3ce9f0
kanghuawu <kang-hua.wu@sjsu.edu>
2018-01-04 11:06:22 -0800

    add extra note to 046

--- Java/src/main/java/leetcode/_046_permutations/LE046PermuteIter.java
+++ Java/src/main/java/leetcode/_046_permutations/LE046PermuteIter.java
@@ -30,4 +30,40 @@ public class LE046PermuteIter {
         LE046PermuteIter t = new LE046PermuteIter();
         System.out.println(t.permute(a));
     }
+
+    // ref: https://leetcode.com/problems/permutations/discuss/18444/?page=1
+//    private static final int[] FACT = { // 479001600 < 2147483647 < 6227020800
+//            1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880, 3628800, 39916800, 479001600
+//    };
+//    public List<List<Integer>> permute(int[] nums) {
+//        Arrays.sort(nums);
+//        List<List<Integer>> result = new ArrayList<>(nums.length);
+//        for (int k = 0; k < FACT[nums.length]; ++k) {
+//            result.add(permutation(nums, k));
+//        }
+//        return result;
+//    }
+//    List<Integer> permutation(int[] nums, int k) {
+//        // k %= FACT[nums.length]; // in case you want to use it elsewhere
+//        List<Integer> source = toList(nums);
+//        List<Integer> result = new ArrayList(nums.length);
+//        while (!source.isEmpty()) {
+//            int f = FACT[source.size() - 1];
+//            result.add(source.remove(k / f));
+//            k %= f;
+//        }
+//        return result;
+//    }
+//    List<Integer> permutation(int[] nums, int k) {
+//        int[] source = nums.clone();
+//        int len = nums.length;
+//        List<Integer> result = new ArrayList(len);
+//        while (len-- > 0) {
+//            int f = FACT[len];
+//            result.add(source[k / f]);
+//            source[k / f] = source[len];
+//            k %= f;
+//        }
+//        return result;
+//    }
 }
