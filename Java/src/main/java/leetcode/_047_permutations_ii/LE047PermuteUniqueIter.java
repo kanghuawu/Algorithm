ba97e88e55e4e77ba9e628b534049e2e6e944ed0
kanghuawu <kang-hua.wu@sjsu.edu>
2018-01-04 16:07:33 -0800

    047

---
+++ Java/src/main/java/leetcode/_047_permutations_ii/LE047PermuteUniqueIter.java
@@ -0,0 +1,36 @@
+package leetcode._047_permutations_ii;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class LE047PermuteUniqueIter {
+    // MY 2017-1-4 ref: https://leetcode.com/problems/permutations-ii/discuss/18630
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> res = new LinkedList<List<Integer>>(){{
+            add(new LinkedList<>());
+        }};
+
+        for (int num : nums) {
+            int size = res.size();
+            for (int i = 0; i < size; i++) {
+                List<Integer> li = res.remove(0);
+                for (int j = 0; j <= li.size(); j++) {
+                    if (j > 0 && li.get(j-1) == num) {
+                        break;
+                    }
+                    List<Integer> per = new LinkedList<>(li);
+                    per.add(j, num);
+                    res.add(per);
+                }
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        LE047PermuteUniqueIter p = new LE047PermuteUniqueIter();
+        int[] arr = new int[]{1,1,2,2};
+        System.out.println(p.permuteUnique(arr));
+    }
+}
