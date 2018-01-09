d5c6a8b3e92b95734d316e27a5d9af8337e6f3a5
kanghuawu <kang-hua.wu@sjsu.edu>
2018-01-08 22:15:13 -0800

    039/040/047/060/216/267

---
+++ Java/src/main/java/leetcode/_216_combination_sum_iii/LE216CombinationSum3.java
@@ -0,0 +1,24 @@
+package leetcode._216_combination_sum_iii;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LE216CombinationSum3 {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(res, new ArrayList<>(), 1, k, n);
+        return res;
+    }
+    private void dfs(List<List<Integer>> res, List<Integer> li, int curr, int k, int n) {
+        if (n < 0 || k < 0) return;
+        if (n == 0 && k == 0) {
+            res.add(new ArrayList<>(li));
+            return;
+        }
+        for (int i = curr; i <= 9; i++) {
+            li.add(i);
+            dfs(res, li, i + 1, k - 1, n - i);
+            li.remove(li.size() - 1);
+        }
+    }
+}
