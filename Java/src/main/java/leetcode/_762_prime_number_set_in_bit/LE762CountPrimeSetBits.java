4dc7dfa8b41524bd594d18ec6a73b01ade3cf8de
kanghuawu <kang-hua.wu@sjsu.edu>
2018-01-13 20:15:03 -0800

    762

---
+++ Java/src/main/java/leetcode/_762_prime_number_set_in_bit/LE762CountPrimeSetBits.java
@@ -0,0 +1,24 @@
+package leetcode._762_prime_number_set_in_bit;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class LE762CountPrimeSetBits {
+    public int countPrimeSetBits(int L, int R) {
+        Set<Integer> primes = new HashSet<Integer>(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31));
+        int count = 0;
+        for (int i = L; i <= R; i++) {
+            int test = i;
+            int ones = 0;
+            while (test != 0) {
+                ones += test & 1;
+                test = test >> 1;
+            }
+            if (primes.contains(ones)) {
+                count += 1;
+            }
+        }
+        return count;
+    }
+}
