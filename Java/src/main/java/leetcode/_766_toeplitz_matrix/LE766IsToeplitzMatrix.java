da72112e6bd7978556dbbdad07d7b3621474c9b3
kanghuawu <kang-hua.wu@sjsu.edu>
2018-01-20 20:49:18 -0800

    1. 766/769 2. inversions

---
+++ Java/src/main/java/leetcode/_766_toeplitz_matrix/LE766IsToeplitzMatrix.java
@@ -0,0 +1,12 @@
+package leetcode._766_toeplitz_matrix;
+
+public class LE766IsToeplitzMatrix {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length-1; i++) {
+            for (int j = 0; j < matrix[0].length-1; j++) {
+                if (matrix[i][j] != matrix[i+1][j+1]) return false;
+            }
+        }
+        return true;
+    }
+}
--- Java/src/main/java/notebook/sort/MergeSort.java
+++ Java/src/main/java/notebook/sort/MergeSort.java
@@ -39,3 +39,44 @@ public class MergeSort implements SortStrategy {
         }
     }
 }
+
+/*
+private int inversions;
+    public int maxChunksToSorted(int[] arr) {
+        inversions = 0;
+        int[] aux = new int[arr.length];
+        sort(arr, aux, 0, arr.length-1);
+        System.out.println(inversions);
+        // if (inversions > arr.length) return 1;
+        System.out.println(Arrays.toString(arr));
+        return arr.length - (inversions+1)/2;
+    }
+
+    private void sort(int[] arr, int[] aux, int lo, int hi) {
+        if (lo >= hi) return;
+        int mid = (lo + hi)/2;
+        sort(arr, aux, lo, mid);
+        sort(arr, aux, mid+1, hi);
+        merge(arr, aux, lo, mid, hi);
+    }
+
+    private void merge(int[] arr, int[] aux, int lo, int mid, int hi) {
+        for (int i = lo; i <= hi; i++) {
+            aux[i] = arr[i];
+        }
+        int j = lo;
+        int k = mid+1;
+        for (int i = lo; i <= hi; i++) {
+            if (j > mid) {
+                arr[i] = aux[k++];
+            } else if (k > hi) {
+                arr[i] = aux[j++];
+            } else if (aux[j] < aux[k]) {
+                arr[i] = aux[j++];
+            } else {
+                inversions += mid - j + 1;
+                arr[i] = aux[k++];
+            }
+        }
+    }
+ */
