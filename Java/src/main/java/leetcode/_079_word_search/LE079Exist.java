d2e8162d24f4334fc5371714d1bc323c7abe17c9
kanghuawu <kang-hua.wu@sjsu.edu>
2018-01-22 00:18:34 -0800

    79/206

--- Java/src/main/java/leetcode/_079_word_search/LE079Exist.java
+++ Java/src/main/java/leetcode/_079_word_search/LE079Exist.java
@@ -1,4 +1,32 @@
 package leetcode._079_word_search;
 
 public class LE079Exist {
+    private int rowL;
+    private int colL;
+    public boolean exist(char[][] board, String word) {
+        rowL = board.length;
+        if (rowL == 0) return false;
+        colL = board[0].length;
+        for (int i = 0; i < rowL; i++) {
+            for (int j = 0; j < colL; j++) {
+                boolean[] visited = new boolean[rowL*colL];
+                if (dfs(board, word, 0, visited, i, j)) return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int strIdx, boolean[] visited, int i, int j) {
+        if (word.length() == strIdx) return true;
+        if (i < 0 || i >= rowL || j < 0 || j >= colL || visited[i*colL + j] || word.charAt(strIdx) != board[i][j]) return false;
+
+        visited[i*colL + j] = true;
+        if (dfs(board, word, strIdx+1, visited, i-1, j) ||
+                dfs(board, word, strIdx+1, visited, i+1, j) ||
+                dfs(board, word, strIdx+1, visited, i, j-1) ||
+                dfs(board, word, strIdx+1, visited, i, j+1)) return true;
+        visited[i*colL + j] = false;
+        return false;
+    }
 }
--- Java/src/main/java/leetcode/_206_implement_trie/LE206TrieHashMap.java
+++ Java/src/main/java/leetcode/_206_implement_trie/LE206TrieHashMap.java
@@ -1,5 +1,53 @@
 package leetcode._206_implement_trie;
 
-public class Trie {
+import java.util.HashMap;
+import java.util.Map;
 
+public class LE206TrieHashMap {
+    private boolean isWord;
+    private Map<Character, LE206TrieHashMap> next;
+    /** Initialize your data structure here. */
+    public LE206TrieHashMap() {
+        isWord = false;
+        next = new HashMap<>();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        if (word.length() == 0) return;
+        insert(this, word, 0);
+    }
+
+    private void insert(LE206TrieHashMap LE206TrieHashMap, String word, int idx) {
+        if (idx == word.length()) {
+            LE206TrieHashMap.isWord = true;
+            return;
+        }
+        LE206TrieHashMap nxtLE206TrieHashMap = LE206TrieHashMap.next.get(word.charAt(idx));
+        if (nxtLE206TrieHashMap == null) {
+            nxtLE206TrieHashMap = new LE206TrieHashMap();
+            LE206TrieHashMap.next.put(word.charAt(idx), nxtLE206TrieHashMap);
+        }
+        LE206TrieHashMap.insert(nxtLE206TrieHashMap, word, idx+1);
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        if (word.length() == 0) return true;
+        LE206TrieHashMap lastLE206TrieHashMap = find(this, word, 0);
+        if (lastLE206TrieHashMap == null || !lastLE206TrieHashMap.isWord) return false;
+        return true;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        if (find(this, prefix, 0) == null) return false;
+        return true;
+    }
+
+    private LE206TrieHashMap find(LE206TrieHashMap LE206TrieHashMap, String s, int idx) {
+        if (LE206TrieHashMap == null) return null;
+        if (s.length() == idx) return LE206TrieHashMap;
+        return find(LE206TrieHashMap.next.get(s.charAt(idx)), s, idx+1);
+    }
 }
