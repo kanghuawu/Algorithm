0a16468b189ddb72705cd1ae4e2335a68789fd95
kanghuawu <kang-hua.wu@sjsu.edu>
2018-01-22 18:19:12 -0800

    206/211

--- Java/src/main/java/leetcode/_206_implement_trie/LE206TrieHashMap.java
+++ Java/src/main/java/leetcode/_206_implement_trie/LE206TrieHashMap.java
@@ -4,50 +4,53 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class LE206TrieHashMap {
-    private boolean isWord;
-    private Map<Character, LE206TrieHashMap> next;
-    /** Initialize your data structure here. */
-    public LE206TrieHashMap() {
-        isWord = false;
-        next = new HashMap<>();
-    }
+    class Trie {
 
-    /** Inserts a word into the trie. */
-    public void insert(String word) {
-        if (word.length() == 0) return;
-        insert(this, word, 0);
-    }
-
-    private void insert(LE206TrieHashMap LE206TrieHashMap, String word, int idx) {
-        if (idx == word.length()) {
-            LE206TrieHashMap.isWord = true;
-            return;
+        private boolean isWord;
+        private Map<Character, Trie> next;
+        /** Initialize your data structure here. */
+        public Trie() {
+            isWord = false;
+            next = new HashMap<>();
         }
-        LE206TrieHashMap nxtLE206TrieHashMap = LE206TrieHashMap.next.get(word.charAt(idx));
-        if (nxtLE206TrieHashMap == null) {
-            nxtLE206TrieHashMap = new LE206TrieHashMap();
-            LE206TrieHashMap.next.put(word.charAt(idx), nxtLE206TrieHashMap);
+
+        /** Inserts a word into the trie. */
+        public void insert(String word) {
+            if (word.length() == 0) return;
+            insert(this, word, 0);
         }
-        LE206TrieHashMap.insert(nxtLE206TrieHashMap, word, idx+1);
-    }
 
-    /** Returns if the word is in the trie. */
-    public boolean search(String word) {
-        if (word.length() == 0) return true;
-        LE206TrieHashMap lastLE206TrieHashMap = find(this, word, 0);
-        if (lastLE206TrieHashMap == null || !lastLE206TrieHashMap.isWord) return false;
-        return true;
-    }
+        private void insert(Trie trie, String word, int idx) {
+            if (idx == word.length()) {
+                trie.isWord = true;
+                return;
+            }
+            Trie nxtTrie = trie.next.get(word.charAt(idx));
+            if (nxtTrie == null) {
+                nxtTrie = new Trie();
+                trie.next.put(word.charAt(idx), nxtTrie);
+            }
+            trie.insert(nxtTrie, word, idx+1);
+        }
 
-    /** Returns if there is any word in the trie that starts with the given prefix. */
-    public boolean startsWith(String prefix) {
-        if (find(this, prefix, 0) == null) return false;
-        return true;
-    }
+        /** Returns if the word is in the trie. */
+        public boolean search(String word) {
+            if (word.length() == 0) return true;
+            Trie lastTrie = find(this, word, 0);
+            if (lastTrie == null || !lastTrie.isWord) return false;
+            return true;
+        }
 
-    private LE206TrieHashMap find(LE206TrieHashMap LE206TrieHashMap, String s, int idx) {
-        if (LE206TrieHashMap == null) return null;
-        if (s.length() == idx) return LE206TrieHashMap;
-        return find(LE206TrieHashMap.next.get(s.charAt(idx)), s, idx+1);
+        /** Returns if there is any word in the trie that starts with the given prefix. */
+        public boolean startsWith(String prefix) {
+            if (find(this, prefix, 0) == null) return false;
+            return true;
+        }
+
+        private Trie find(Trie trie, String s, int idx) {
+            if (trie == null) return null;
+            if (s.length() == idx) return trie;
+            return find(trie.next.get(s.charAt(idx)), s, idx+1);
+        }
     }
 }
---
+++ Java/src/main/java/leetcode/_206_implement_trie/LE206TrieTST.java
@@ -0,0 +1,62 @@
+package leetcode._206_implement_trie;
+
+public class LE206TrieTST {
+    class Trie {
+        private class Node {
+            private char ch;
+            private Node left, mid, right;
+            private boolean isWord;
+        }
+
+        private Node root;
+        /** Initialize your data structure here. */
+        public Trie() {
+        }
+
+        /** Inserts a word into the trie. */
+        public void insert(String word) {
+            root = insert(root, word, 0);
+        }
+
+        private Node insert(Node node, String word, int i) {
+            // System.out.println(i);
+            char ch = word.charAt(i);
+            if (node == null) {
+                node = new Node();
+                node.ch = ch;
+            }
+
+            if (ch > node.ch) node.right = insert(node.right, word, i);
+            else if (ch < node.ch) node.left =  insert(node.left, word, i);
+            else if (word.length() - 1 == i) node.isWord = true;
+            else node.mid = insert(node.mid, word, i+1);
+            return node;
+        }
+
+        /** Returns if the word is in the trie. */
+        public boolean search(String word) {
+            Node lastNode = find(root, word, 0);
+            if (lastNode == null) return false;
+            // System.out.println(lastNode.isWord);
+            return lastNode.isWord;
+        }
+
+        /** Returns if there is any word in the trie that starts with the given prefix. */
+        public boolean startsWith(String prefix) {
+            Node lastNode = find(root, prefix, 0);
+            return lastNode != null;
+        }
+
+        private Node find(Node node, String word, int i) {
+            // System.out.println(i);
+            // System.out.println(node.ch);
+            if (node == null) return null;
+            char ch = word.charAt(i);
+
+            if (ch > node.ch) return find(node.right, word, i);
+            else if (ch < node.ch) return find(node.left, word, i);
+            else if (word.length()-1 == i) return node;
+            return find(node.mid, word, i+1);
+        }
+    }
+}
