06ab0405c56e29d7e8a028d46b63239fbd746e11
kanghuawu <kang-hua.wu@sjsu.edu>
2018-01-25 21:55:10 -0800

    1. bring out Trie 2. fix scoreOf

--- AlgorithmsCoursera/src/main/java/p24_boggle/BoggleSolver.java
+++ AlgorithmsCoursera/src/main/java/p24_boggle/BoggleSolver.java
@@ -3,85 +3,26 @@ package p24_boggle;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class BoggleSolver
 {
-    private Trie trie;
+    private final Trie trie;
+    private final TreeSet<String> dictionary;
     // Initializes the data structure using the given array of strings as the dictionary.
     // (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
     public BoggleSolver(String[] dictionary) {
+        this.dictionary = new TreeSet<>();
         trie = new Trie();
         for (String s : dictionary) {
+            this.dictionary.add(s);
             if (s.contains("QU")) s = s.replaceAll("QU", "Q");
             else if (s.contains("Q")) continue;
             trie.insert(s);
-        }
-    }
-
-
-    private class Trie {
-        Node root;
-        int rowL = -1;
-        int colL = -1;
-        class Node {
-            char ch;
-            boolean isWord;
-            Node left, mid, right;
-        }
-        Trie() {}
-
-        public void insert(String word) {
-            root = insert(root, word, 0);
-        }
-
-        private Node insert(Node node, String word, int i) {
-            char ch = word.charAt(i);
-            if (node == null) {
-                node = new Node();
-                node.ch = ch;
-            }
-            if (ch > node.ch) node.right = insert(node.right, word, i);
-            else if (ch < node.ch) node.left =  insert(node.left, word, i);
-            else if (word.length() - 1 == i) node.isWord = true;
-            else node.mid = insert(node.mid, word, i+1);
-            return node;
-        }
-
-        public void search(char[][] board, int i, int j, Set<String> res) {
-            if (rowL == -1 || colL == -1) throw new IllegalArgumentException();
-            find(root, board, i, j, new StringBuilder(), res, new boolean[rowL][colL]);
-        }
-
-        private void find(Node node, char[][] board, int i, int j, StringBuilder sb, Set<String> res, boolean[][] visited) {
-            if (node == null || i < 0 || i >= rowL || j < 0 || j >= colL || visited[i][j]) return;
-
-            char ch = board[i][j];
-
-            if (ch > node.ch) {
-                find(node.right, board, i, j, sb, res, visited);
-                return;
-            } else if (ch < node.ch) {
-                find(node.left, board, i, j, sb, res, visited);
-                return;
-            }
-            visited[i][j] = true;
-
-            sb.append(ch);
-            if (node.isWord) {
-                res.add(sb.toString());
-            }
-
-            for (int dRow = -1; dRow <= 1; dRow++) {
-                for (int dCol = -1; dCol <= 1; dCol++) {
-                    if (dRow == 0 && dCol == 0) continue;
-                    find(node.mid, board, i+dRow, j+dCol, sb, res, visited);
-                }
-            }
-
-            visited[i][j] = false;
-            sb.deleteCharAt(sb.length()-1);
         }
     }
 
@@ -90,8 +31,8 @@ public class BoggleSolver
         int rowL = board.rows();
         int colL = board.cols();
         char[][] boardChar = new char[rowL][colL];
-        trie.rowL = rowL;
-        trie.colL = colL;
+        trie.setRowL(rowL);
+        trie.setColL(colL);
         for (int row = 0; row < rowL; row++) {
             for (int col = 0; col < colL; col++) {
                 boardChar[row][col] = board.getLetter(row, col);
@@ -103,18 +44,21 @@ public class BoggleSolver
                 trie.search(boardChar, row, col, res);
             }
         }
+        List<String> resReplacement = new ArrayList<>();
         for (String s : res) {
-            if (s.contains("Q")) {
-                res.add(s.replaceAll("Q", "QU"));
-                res.remove(s);
+            if (s.contains("Q") && s.length() >= 2) {
+                resReplacement.add(s.replaceAll("Q", "QU"));
+            } else if (s.length() >= 3) {
+                resReplacement.add(s);
             }
         }
-        return res;
+        return resReplacement;
     }
 
     // Returns the score of the given word if it is in the dictionary, zero otherwise.
     // (You can assume the word contains only the uppercase letters A through Z.)
     public int scoreOf(String word) {
+        if (!dictionary.contains(word)) return 0;
         int len = word.length();
         if (len <=2 ) return 0;
         else if (len <= 4) return 1;
@@ -126,7 +70,7 @@ public class BoggleSolver
 
     public static void main(String[] args) {
         String path1 = "/Users/khwu/Projects/Algorithm/AlgorithmsCoursera/src/main/java/p24_boggle/test/dictionary-yawl.txt";
-        String path2 = "/Users/khwu/Projects/Algorithm/AlgorithmsCoursera/src/main/java/p24_boggle/test/board-points26539.txt";
+        String path2 = "/Users/khwu/Projects/Algorithm/AlgorithmsCoursera/src/main/java/p24_boggle/test/board-points777.txt";
         In in = new In(path1);
         String[] dictionary = in.readAllStrings();
         BoggleSolver solver = new BoggleSolver(dictionary);
