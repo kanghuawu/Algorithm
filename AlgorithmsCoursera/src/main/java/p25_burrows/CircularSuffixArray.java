f3043be9afbbf5244be62c2ec4f595b7f6d4cb73
kanghuawu <kang-hua.wu@sjsu.edu>
2018-01-28 18:31:45 -0800

    burrow init

---
+++ AlgorithmsCoursera/src/main/java/p25_burrows/CircularSuffixArray.java
@@ -0,0 +1,23 @@
+package p25_burrows;
+
+public class CircularSuffixArray {
+    // circular suffix array of s
+    public CircularSuffixArray(String s) {
+
+    }
+
+    // length of s
+    public int length() {
+        return 0;
+    }
+
+    // returns index of ith sorted suffix
+    public int index(int i) {
+        return 0;
+    }
+
+    // unit testing (required)
+    public static void main(String[] args) {
+
+    }
+}
---
+++ AlgorithmsCoursera/src/main/java/p25_burrows/MoveToFront.java
@@ -0,0 +1,16 @@
+package p25_burrows;
+
+public class MoveToFront {
+    // apply move-to-front encoding, reading from standard input and writing to standard output
+    public static void encode() {
+
+    }
+    // apply move-to-front decoding, reading from standard input and writing to standard output
+    public static void decode() {
+
+    }
+    // if args[0] is '-', apply move-to-front encoding // if args[0] is '+', apply move-to-front decoding
+    public static void main(String[] args) {
+
+    }
+}
